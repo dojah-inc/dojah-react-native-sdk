b537dece13dbd99d09ed754dd91a50dda0e47d9b
shittu33 <shittuabdulmujeeb@gmail.com>
2025-03-23 19:12:42 +0100

    fix: android & ios

--- src/index.tsx
+++ src/index.tsx
@@ -17,24 +17,28 @@ const DojahKyc = NativeModules.DojahKyc
       }
     );
 
-/// Initialize Dojah KYC for IOS, 
+/// Initialize Dojah KYC for IOS,
 /// call this before your app is registered
 /// the Sdk won't work on IOS without this
 /// @param appName: the name of your app
 // export function initializeDojahIOS(appName: string) {
-  // if(Platform.OS === 'ios'){
-  //   DojahKyc.initialize(appName);
-  // }
+// if(Platform.OS === 'ios'){
+//   DojahKyc.initialize(appName);
+// }
 // }
 
-export function launchDojahKyc(widgetId: string, referenceId?:string|null, email?: string|null) {
-  if(Platform.OS === 'ios'){
-    DojahKyc.launch(widgetId,referenceId??"",email??"");
-  }else{
-    DojahKyc.launch(widgetId,referenceId,email);
+export function launchDojahKyc(
+  widgetId: string,
+  referenceId?: string | null,
+  email?: string | null
+) {
+  if (Platform.OS === 'ios') {
+    DojahKyc.launch(widgetId, referenceId ?? '', email ?? '');
+  } else {
+    DojahKyc.launch(widgetId, referenceId, email);
   }
 }
 
-export function getIdHistory(): Promise<Map<string,string> | null> {
+export function getIdHistory(): Promise<Map<string, string> | null> {
   return DojahKyc.getIdHistory();
 }
